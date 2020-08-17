28d965e7afb796ab36b536d2efb4e38898c3c8dd
Taylor Allen <tjaroguetech@gmail.com>
2020-08-17 13:45:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,5 +18,7 @@ def speak_to_grandma(phrase)
   elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
     
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
   end
 end
